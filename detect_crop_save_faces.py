f043f0df500af56c86c86248f11005d63d3951e5
iykenny <imanziyannickkenny@gmail.com>
2023-05-09 22:14:10 +0200

    face detection code

--- detect_crop_save_faces.py
+++ detect_crop_save_faces.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread("/home/iykenny/Downloads/couple.jpg")
+img = cv2.imread("/home/iykenny/Desktop/faceDetection/couple.jpg")
 
 image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
